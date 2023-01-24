9156c80c35c322a6a6532686e1080300553e5b92
Luiz Gabriel <luizgabrielgrupe@gmail.com>
2023-01-24 18:02:41 -0300

    feat(dbConnection): create database connection

---
+++ src/config/dbConnection.mjs
@@ -0,0 +1,17 @@
+import Sequelize from 'sequelize';
+import getConfig from '../config/configCondition.mjs';
+
+const config = getConfig()
+
+const sequelize = new Sequelize(config);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    // TODO: Add code to log in to your database
+  })
+  .catch(err => {
+    // TODO: Add code to error log in to your database
+  });
+
+export default sequelize;
