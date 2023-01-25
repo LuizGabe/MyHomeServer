b21b9df91b3ec130c6d9ac3335670d9fd9860793
Luiz Gabriel <luizgabrielgrupe@gmail.com>
2023-01-25 04:19:53 -0300

    refactor(models): refactoring the insert data in temperature

--- src/models/temperature/insert.mjs
+++ src/models/temperature/insert.mjs
@@ -1,17 +1,11 @@
 import Temperature from "./temperature.mjs";
 
-function select() {
-
   const insertData = (data) => {
-    const jsonData = JSON.parse(data)
-
-    Temperature.create(jsonData).then(() => {
+    Temperature.create(data).then(() => {
       // TODO: Add code to insert log in to your database
     }).catch(err => {
       // TODO: Add code to insert error log in to your database
     })
   }
 
-  return { insertData }
-}
-export default select
+  export { insertData }
