6ffc824ab43865f50e7eb2a21ffaae647b23674c
Luiz Gabriel <luizgabrielgrupe@gmail.com>
2023-01-25 02:40:19 -0300

    refactor(models): refactoring the export module

--- src/models/temperature/insert.mjs
+++ src/models/temperature/insert.mjs
@@ -1,12 +1,17 @@
 import Temperature from "./temperature.mjs";
 
-function insertData(data) {
-  const jsonData = JSON.parse(data);
+function select() {
 
-  Temperature.create(jsonData).then(() => {
-    // TODO: Add code to insert log in to your database
-  }).catch(err => {
-    // TODO: Add code to insert error log in to your database
-  })
+  const insertData = (data) => {
+    const jsonData = JSON.parse(data)
+
+    Temperature.create(jsonData).then(() => {
+      // TODO: Add code to insert log in to your database
+    }).catch(err => {
+      // TODO: Add code to insert error log in to your database
+    })
+  }
+
+  return { insertData }
 }
-export default insertData;
+export default select
