d004a03db9a3f8add31f03c20f4801936628e7b0
Luiz Gabriel <luizgabrielgrupe@gmail.com>
2023-02-04 12:31:13 -0300

    fix(hello): additional 1 on 1 instead of 2 on 2

--- src/controllers/helloController.mjs
+++ src/controllers/helloController.mjs
@@ -4,8 +4,9 @@ const { logInfo } = new Logger()
 let count = 0
 
 const printHello = (req, res) => {
-  logInfo(`Hello World! ${count++}`, 'helloController')
-  res.send(`Hello World! ${count++}`)
+  count++
+  logInfo(`Hello World! ${count}`, 'helloController')
+  res.send(`Hello World! ${count}`)
 }
 
 export { printHello }
