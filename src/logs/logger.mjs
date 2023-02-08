c0606ffe6b39d49d01e4fac01cafc8e0e567e5dc
Luiz Gabriel <luizgabrielgrupe@gmail.com>
2023-02-08 01:28:58 -0300

    feat(logs): create describe logger function for developer

--- src/logs/logger.mjs
+++ src/logs/logger.mjs
@@ -1,30 +1,31 @@
 const red = '\x1b[31m'
 const green = '\x1b[32m'
 const yellow = '\x1b[33m'
+const blue = '\x1b[34m';
 
 const reset = '\x1b[0m'
 
 class Logger {
 
   logError(error, file) {
-    console.log(red, `[ERROR] in ${file}: ${error}`, reset)
+    console.log(red, `[ERROR] in ${file}: `, error, reset)
   }
 
   logWarning(warning, file) {
-    console.warn(yellow, `[WARNING] in ${file}: ${warning}`, reset)
+    console.warn(yellow, `[WARNING] in ${file}: `, warning, reset)
   }
 
   logInfo(info, file) {
-    console.info(` [INFO] in ${file}: ${info}`)
+    console.info(` [INFO] in ${file}:`, info)
   }
 
   logSuccess(success, file) {
-    console.log(green, `[SUCCESS] in ${file}: ${success}`, reset)
+    console.log(green, `[SUCCESS] in ${file}: `, success, reset)
+  }
   
+  logDescribe(title, description) {
+    console.log(blue,'\x1b[1m', `${title}:`, '\x1b[0m', description)
   }
 }
-
-
-
 
 export default Logger
